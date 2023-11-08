64aa827cf8d689aff7a0f0eb62e317e312df3a70
Zenlexi <lang.kaelyn@gmail.com>
2023-11-08 12:34:11 -0600

    Started building structure for dog-card component

--- src/app/dog-card/dog-card.component.ts
+++ src/app/dog-card/dog-card.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./dog-card.component.css']
 })
 export class DogCardComponent {
+  dogName: string;
+  dogBreed: string;
+  dogAge: number;
+  dogImage: string;
+  dogZip: string;
+  dogId: string;
 
+  constructor(_id:string, _img:string, _name:string, _age:number, _zip_code:string, _breed:string) {
+    this.dogId = _id;
+    this.dogImage = _img;
+    this.dogName = _name;
+    this.dogAge = _age;
+    this.dogZip = _zip_code;
+    this.dogBreed = _breed;
+  }
 }
