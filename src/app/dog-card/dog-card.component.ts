ff8db0d7d017c2d48568a8a8b952e801c2fb25af
Zenlexi <lang.kaelyn@gmail.com>
2023-11-08 18:29:44 -0600

    Dog cards now show up

--- src/app/dog-card/dog-card.component.ts
+++ src/app/dog-card/dog-card.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Dog } from '../dog';
-import { DogService } from '../dog.service';
 
 @Component({
   selector: 'app-dog-card',
   templateUrl: './dog-card.component.html',
   styleUrls: ['./dog-card.component.css']
 })
-export class DogCardComponent{
 
+export class DogCardComponent{
+  @Input() dog?: Dog;
 }
