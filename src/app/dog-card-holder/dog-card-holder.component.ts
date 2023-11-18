1d334adc7d8942cc4ed60f4714078567973979f4
Zenlexi <lang.kaelyn@gmail.com>
2023-11-18 16:20:40 -0600

    Fixed paging

--- src/app/dog-card-holder/dog-card-holder.component.ts
+++ src/app/dog-card-holder/dog-card-holder.component.ts
@@ -17,8 +17,6 @@ interface SortOptions {
 })
 export class DogCardHolderComponent implements OnInit {
 
-  dogList: Dog[] = [];
-  totalDogs: number = 0;
   sortOptions: SortOptions[] = [
     {text:"Breed (Ascending)", apiString:"breed:asc", selected: true},
     {text:"Breed (Descending)", apiString:"breed:desc", selected: false},
@@ -31,41 +29,24 @@ export class DogCardHolderComponent implements OnInit {
   constructor(public dogService: DogService) {}
 
   ngOnInit() {
-    this.loadAllDogs();
-  }
-
-  loadAllDogs = async () => {
-    // Load all dogs by default
     this.dogService.getAllDogs();
   }
 
   handlePageEvent = async (e: PageEvent) => {
     if (e.previousPageIndex === undefined || e.pageIndex > e.previousPageIndex) {
-      try {
-        // this.dogList = await this.dogService.nextDogPage();
-      } catch (error) {
-        this.dogList = [];
-        console.error(error);
-      }
+      this.dogService.nextDogPage();
     } else if (e.previousPageIndex > e.pageIndex) {
-      try {
-        // this.dogList = await this.dogService.prevDogPage();
-      } catch (error) {
-        this.dogList = [];
-        console.error(error);
-      }
+      this.dogService.prevDogPage();
     }
   }
 
   sortDogs = async (e: any) => {
     if (e.target.value == this.sortOptions[0].apiString) {
-      this.loadAllDogs();
+      this.dogService.getAllDogs();
     } else {
       try {
         // this.dogList = await this.dogService.getSortedDogs(e.target.value);
-        this.totalDogs = this.dogService.searchResult.total;
       } catch (error) {
-        this.dogList = [];
         console.error(error);
       }
     }
